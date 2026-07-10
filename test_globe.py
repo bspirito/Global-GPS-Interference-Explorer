b5e42b29eece9c0d9272dea18828dc98cb662076
bspirito <bspirito@example.com>
2026-07-10 01:29:25 -0400

    Remove test data and images

--- test_globe.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-import pydeck as pdk
-deck = pdk.Deck(views=[pdk.View(type='_GlobeView', controller=True)], map_provider='carto')
-st.components.v1.html(deck.to_html(as_string=True), height=600)
